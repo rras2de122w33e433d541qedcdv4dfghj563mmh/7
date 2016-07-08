a7490b4e7510ff4f21bc05d96cb0919df47b31ea
MohammadReza <hackerfirefox@yahoo.com>
2016-07-08 21:52:07 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '180163286:AAHBK3l2P9a3GQXCn3FWGDozs4Y1BCjrkO4');
+define('BOT_TOKEN', 'YourBotToken');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
